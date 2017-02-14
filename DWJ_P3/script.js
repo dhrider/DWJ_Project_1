a2bfd5b3f1bb2d82008c54e378d3e80c6bb09a10
Hetphil <p_bordmann@orange.fr>
2017-02-14 10:18:05 +0100

    - initialisation Google maps sur Paris

---
+++ DWJ_P3/script.js
@@ -0,0 +1,7 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 48.862725, lng: 2.287592000000018},
+        zoom: 11
+    });
+}
